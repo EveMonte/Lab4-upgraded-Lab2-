c356df8abb93679957112106b3dc9ee8265565cf
Дмитрий Савельев <mrpresident.evmount@gmail.com>
2021-02-20 17:17:32 +0300

    Damn stupid patterns. Don't forget to change strings in switch-case to enum. Idea is to combine Builder pattern with Abstract Factory. I want to call Builders from Factories. So stupid, but...

---
+++ Лаба2/Form1.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml.Serialization;
+using System.Threading;
+
+
+namespace Лаба2
+{
+    public partial class Form1 : Form
+    {
+        List<Discipline> listOfDisciplines = new List<Discipline>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SendAll_Click(object sender, EventArgs e)
+        {
+            string key = Speciality.SelectedItem.ToString();
+            switch (key)
+            {
+                case "ИСиТ":
+                    new Client().ClientMethod(new ISITFactory(this));
+                    break;
+                case "ПОИТ":
+                    new Client().ClientMethod(new POITFactory());
+                    break;
+                case "ПОИБМС":
+                    new Client().ClientMethod(new POIBMSFactory());
+                    break;
+                case "ДЭиВИ":
+                    new Client().ClientMethod(new DEWPFactory());
+                    break;
+            }
+            //new Client().ClientMethod();
+
+            Discipline discipline = new Discipline();
+            #region Discipline form
+            try
+            {
+                if (DisciplineName.Text != "")
+                {
+                    DisciplineName.BackColor = Color.White;
+                    discipline.Name = DisciplineName.Text;
+
+                }
+                else
+                {
+                    DisciplineName.BackColor = Color.Salmon;
+                    throw new Exception("Please enter Name!");
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorLabel.Text = ex.Message;
+            }
+            discipline.NumberOfLaboratories = NumberOfLaboratories.Value;
+            discipline.NumberOfLections = NumberOfLections.Value;
+            discipline.Semestr = Int32.Parse(Semestr.Text);
+            discipline.Year = (int)Year.Value;
+            discipline.Speciality = Speciality.SelectedItem.ToString();
+
+            try
+            {
+                if (Offset.Checked && !Exam.Checked)
+                {
+                    discipline.TypeOfControl = Offset.Text;
+
+                }
+                else if (!Offset.Checked && Exam.Checked)
+                {
+                    discipline.TypeOfControl = Exam.Text;
+
+                }
+                else if (!Offset.Checked && !Exam.Checked)
+                {
+                    throw new Exception("Please click on radio-button!");
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorLabel.Text = ex.Message;
+            }
+            #endregion
+
+            #region Lecturer form
+            try
+            {
+                if (SecondName.Text != "")
+                {
+                    discipline.lecturer.SecondName = SecondName.Text;
+                    SecondName.BackColor = Color.White;
+                }
+                else
+                {
+                    SecondName.BackColor = Color.Salmon;
+                    throw new Exception("Fill the text box/boxes!");
+                }
+            }
+            catch(Exception ex)
+            {
+                ErrorLabel.Text = ex.Message;
+            }
+            try
+            {
+                if (NameOfLecturer.Text != "")
+                {
+                    discipline.lecturer.Name = NameOfLecturer.Text;
+                    NameOfLecturer.BackColor = Color.White;
+                }
+                else
+                {
+                    NameOfLecturer.BackColor = Color.Salmon;
+                    throw new Exception("Fill the text box/boxes!");
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorLabel.Text = ex.Message;
+            }
+
+            try
+            {
+                if (NameOfLecturer.Text != "")
+                {
+                    discipline.lecturer.Patronymic = Patronymic.Text;
+                    Patronymic.BackColor = Color.White;
+                }
+                else
+                {
+                    Patronymic.BackColor = Color.Salmon;
+                    throw new Exception("Fill the text box/boxes!");
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorLabel.Text = ex.Message;
+            }
+
+            try
+            {
+                if (Chair.Text != "")
+                {
+                    discipline.lecturer.Chair = Chair.Text;
+                    Chair.BackColor = Color.White;
+                }
+                else
+                {
+                    Chair.BackColor = Color.Salmon;
+                    throw new Exception("Fill the text box/boxes!");
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorLabel.Text = ex.Message;
+            }
+
+            try
+            {
+                if (Auditory.Text != "")
+                {
+                    discipline.lecturer.Auditory = Auditory.Text;
+                    Auditory.BackColor = Color.White;
+                }
+                else
+                {
+                    Auditory.BackColor = Color.Salmon;
+                    throw new Exception("Fill the text box/boxes!");
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorLabel.Text = ex.Message;
+            }
+            #endregion
+
+            #region List of literature form
+            Book book = new Book();
+
+            try
+            {
+                if (NameOfBook.Text != "")
+                {
+                    book.Name = NameOfBook.Text;
+                    NameOfBook.BackColor = Color.White;
+                }
+                else
+                {
+                    NameOfBook.BackColor = Color.Salmon;
+                    throw new Exception("Fill the text box/boxes!");
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorLabel.Text = ex.Message;
+            }
+            try
+            {
+                if (NameOfBook.Text != "")
+                {
+                    book.Author = Author.Text;
+                    Author.BackColor = Color.White;
+                }
+                else
+                {
+                    Author.BackColor = Color.Salmon;
+                    throw new Exception("Fill the text box/boxes!");
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorLabel.Text = ex.Message;
+            }
+            try
+            {
+                if (dateTimePicker1 != null)
+                {
+                    book.date = dateTimePicker1.Value;
+                    //NameOfBook.BackColor = Color.White;
+                }
+                else
+                {
+                    //NameOfBook.BackColor = Color.Salmon;
+                    throw new Exception("Fill the text box/boxes!");
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorLabel.Text = ex.Message;
+            }
+            discipline.listOfLiterature = book;
+
+            #endregion
+
+            listOfDisciplines.Add(discipline);
+            XmlSerializer xser = new XmlSerializer(typeof(List<Discipline>));
+            using (FileStream fileStream = new FileStream("Forms.xml", FileMode.Create))
+            {
+                xser.Serialize(fileStream, listOfDisciplines);
+            }
+        }
+
+        private void NumberOfLections_Scroll(object sender, EventArgs e)
+        {
+            label19.Text = String.Format("Значение: {0}", NumberOfLections.Value);
+        }
+
+        private void NumberOfLaboratories_Scroll(object sender, EventArgs e)
+        {
+            label20.Text = String.Format("Значение: {0}", NumberOfLaboratories.Value);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            XmlSerializer xser = new XmlSerializer(typeof(List<Discipline>));
+            using (FileStream fileStream = new FileStream("Forms.xml", FileMode.OpenOrCreate))
+            {
+                xser.Serialize(fileStream, listOfDisciplines);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            myForm form = new myForm();
+            form.Show();
+        }
+    }
+}
---
+++ Лаба2/Program.cs
@@ -0,0 +1,340 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Лаба2
+{
+    public interface IBuilder
+    {
+        void BuildDisciplineNameAndTypePart(string disName, string typeOfControl);
+        void BuildNumbersPart(int lections, int laboratories);
+        void BuildProgressAndSpecialityPart(int year, int semestr, string speciality);
+        void BuildLecturerFIOPart(string second, string name, string patronymic);
+        void BuildLecturerAnotherPart(string chair, string auditory);
+        void BuildBookPart(string name, string author, DateTime dateTime);
+    }
+
+    public class ConcreteBuilder : IBuilder
+    {
+        private Discipline discipline = new Discipline();
+        public ConcreteBuilder(Discipline dis)
+        {
+            this.Reset();
+            discipline = dis;
+        }
+        public void Reset()
+        {
+            this.discipline = new Discipline();
+        }
+
+        public void BuildDisciplineNameAndTypePart(string disName, string typeOfControl)
+        {
+            discipline.Name = disName;
+            discipline.TypeOfControl = typeOfControl;
+        }
+        public void BuildNumbersPart(int lections, int laboratories)
+        {
+            discipline.NumberOfLections = lections;
+            discipline.NumberOfLaboratories = laboratories;
+        }
+        public void BuildProgressAndSpecialityPart(int year, int semestr, string speciality)
+        {
+            discipline.Year = year;
+            discipline.Semestr = semestr;
+            discipline.Speciality = speciality;
+        }
+        public void BuildLecturerFIOPart(string second, string name, string patronymic)
+        {
+            discipline.lecturer.SecondName = second;
+            discipline.lecturer.Name = name;
+            discipline.lecturer.Patronymic = patronymic;
+        }
+        public void BuildLecturerAnotherPart(string chair, string auditory)
+        {
+            discipline.lecturer.Chair = chair;
+            discipline.lecturer.Auditory = auditory;
+        }
+        public void BuildBookPart(string name, string author, DateTime dateTime)
+        {
+            discipline.listOfLiterature.Name = name;
+            discipline.listOfLiterature.Author = author;
+            discipline.listOfLiterature.date = dateTime;
+        }
+
+        public Discipline GetResult()
+        {
+            Discipline result = this.discipline;
+            this.Reset();
+            return result;
+        }
+    }
+    interface IAbstractFactory
+    {
+        IAbstractDiscipline CreateDiscipline();
+        IAbstractLecturer CreateLecturer();
+        IAbstractBook CreateBook();
+    }
+
+    interface IAbstractDiscipline
+    {
+        void AddLecturer(IAbstractLecturer lecturer);
+        void AddBook(IAbstractBook book);
+        string Name
+        {
+            get;
+            set;
+        }
+        int Semestr
+        {
+            get;
+            set;
+        }
+        int Year
+        {
+            get;
+            set;
+        }
+        string Speciality
+        {
+            get;
+            set;
+        }
+        int NumberOfLections
+        {
+            get;
+            set;
+        }
+        int NumberOfLaboratories { get; set; }
+        string TypeOfControl { get; set; }
+
+    }
+
+    public interface IAbstractLecturer
+    {
+        string Chair { get; set; }//such an interesting translation by Google Translator
+        string SecondName { get; set; }
+        string Name { get; set; }
+        string Patronymic { get; set; }
+        string Auditory { get; set; }
+    }
+
+    public interface IAbstractBook
+    {
+        string Name { get; set; }
+        string Author { get; set; }
+        DateTime date { get; set; }
+    }
+
+    [Serializable]
+    public class Book : IAbstractBook
+    {
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public DateTime date { get; set; }
+    }
+
+    static class Finder 
+    { 
+        static public string FindElementsInForm(string elementsName, string elementsType, Form1 form)
+        {
+            switch (elementsType)
+            {
+                case "TextBox":
+                    foreach(TextBox textBox in form.Controls)
+                    {
+                        if(textBox.Name == elementsName)
+                        {
+                            return textBox.Text;
+                        }
+                    }
+
+                    break;
+                case "ComboBox":
+                    foreach (ComboBox comboBox in form.Controls)
+                    {
+                        if (comboBox.Name == elementsName)
+                        {
+                            return comboBox.Text;
+                        }
+                    }
+                    break;
+                case "ListBox":
+                    foreach (ListBox listBox in form.Controls)
+                    {
+                        if (listBox.Name == elementsName)
+                        {
+                            return listBox.SelectedItem.ToString();
+                        }
+                    }
+                    break;
+                case "NumericUpDown":
+                    foreach (NumericUpDown numericUpDown in form.Controls)
+                    {
+                        if (numericUpDown.Name == elementsName)
+                        {
+                            return numericUpDown.Text;
+                        }
+                    }
+                    break;
+            }
+            return "";
+        }
+    }
+
+    
+    class ISITFactory : IAbstractFactory
+    {
+        Discipline dis;
+        ConcreteBuilder concreteBuilder;
+        Form1 form;
+        public ISITFactory(Form1 form1)
+        {
+            dis = new Discipline();
+            concreteBuilder = new ConcreteBuilder(dis);
+            form = form1;
+        }
+
+        public IAbstractDiscipline CreateDiscipline()
+        {
+            concreteBuilder.BuildDisciplineNameAndTypePart(form.);
+            concreteBuilder.BuildNumbersPart();
+            concreteBuilder.BuildProgressAndSpecialityPart();
+
+        }
+        public IAbstractLecturer CreateLecturer()
+        {
+            return new Lecturer();
+
+        }
+        public IAbstractBook CreateBook()
+        {
+            return new Book();
+
+        }
+    }
+    class POITFactory : IAbstractFactory
+    {
+        public IAbstractDiscipline CreateDiscipline()
+        {
+            return new Discipline();
+        }
+        public IAbstractLecturer CreateLecturer()
+        {
+            return new Lecturer();
+        }
+        public IAbstractBook CreateBook()
+        {
+            return new Book();
+        }
+    }
+    class POIBMSFactory : IAbstractFactory
+    {
+        public IAbstractDiscipline CreateDiscipline()
+        {
+            return new Discipline();
+        }
+        public IAbstractLecturer CreateLecturer()
+        {
+            return new Lecturer();
+        }
+        public IAbstractBook CreateBook()
+        {
+            return new Book();
+        }
+    }
+    class DEWPFactory : IAbstractFactory
+    {
+        public IAbstractDiscipline CreateDiscipline()
+        {
+            return new Discipline();
+
+        }
+        public IAbstractLecturer CreateLecturer()
+        {
+            return new Lecturer();
+        }
+        public IAbstractBook CreateBook()
+        {
+            return new Book();
+        }
+    }
+
+    [Serializable]
+    public class Lecturer : IAbstractLecturer
+    {
+        public string Chair { get; set; }//such an interesting translation by Google Translator
+        public string SecondName { get; set; }
+        public string Name { get; set; }
+        public string Patronymic { get; set; }
+        public string Auditory { get; set; }
+    }
+    [Serializable]
+    public class Discipline : IAbstractDiscipline
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+        public int Semestr
+        {
+            get;
+            set;
+        }
+        public int Year
+        {
+            get;
+            set;
+        }
+        public string Speciality
+        {
+            get;
+            set;
+        }
+        public int NumberOfLections
+        {
+            get;
+            set;
+        }
+        public int NumberOfLaboratories { get; set; }
+        public string TypeOfControl { get; set; }
+
+        public Lecturer lecturer;
+        public Book listOfLiterature;
+        public void AddLecturer(IAbstractLecturer abstractLecturer)
+        {
+            lecturer = (Lecturer)abstractLecturer;
+        }
+        public void AddBook(IAbstractBook abstractBook)
+        {
+            listOfLiterature = (Book)abstractBook;
+        }
+
+
+    }
+    class Client
+    {
+        public void ClientMethod(IAbstractFactory factory)
+        {
+            var productLecturer = factory.CreateLecturer();
+            var productBook = factory.CreateBook();
+            var productDiscipline = factory.CreateDiscipline();
+        }
+    }
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+
+}
